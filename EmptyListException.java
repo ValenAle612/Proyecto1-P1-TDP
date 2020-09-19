2d5ead9dbcdfde279c107d9113502299f660afc7
ValenAle612 <valenale612@gmail.com>
2020-09-18 21:03:40 -0300

    commit tres

--- EmptyListException.java
+++ EmptyListException.java
@@ -10,7 +10,7 @@ public class EmptyListException extends Exception{
 	 * al manipularse una lista vacia en un caso no deseado
 	 * @param msg mensaje que mostrara al lanzarse la excepcion
 	 */
-	public EmptyListException(String msg) {
+	public EmptyListException(String msg, String hola) {
 		super(msg);
 	}
 }
